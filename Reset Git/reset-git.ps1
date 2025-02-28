eef2b04d4ee07e2a1892fd30b4ba6476a401ce26
Patrick Goodwin <pattygsocials@gmail.com>
2025-02-28 15:17:53 -0500

    Update reset-git.ps1

--- Reset Git/reset-git.ps1
+++ Reset Git/reset-git.ps1
@@ -1,5 +1,12 @@
 function Reset-Git {
+	Write-Host("reset-git: Fetching...") -ForegroundColor Yellow
     git fetch
+	
+	Write-Host("reset-git: Resetting branch to original state...") -ForegroundColor Yellow
     git reset --hard origin/main
+	
+	Write-Host ("reset-git: Pushing changes...") -ForegroundColor Yellow
     git push --force
+	
+	Write-Host("reset-git: Reset git complete.") -ForegroundColor Green
 }
